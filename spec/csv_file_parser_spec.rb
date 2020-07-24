313ae3a944846c947408aa053b5313c708b8aa51
Jeff Wong <jeffmjwong@gmail.com>
2020-07-24 21:20:11 +1000

    use let syntax for csv file parser spec

--- spec/csv_file_parser_spec.rb
+++ spec/csv_file_parser_spec.rb
@@ -1,9 +1,13 @@
 require './src/csv_file_parser'
 
-RSpec.describe 'CsvFileParser' do
-  describe 'read' do
+RSpec.describe CsvFileParser do
+  describe '.read' do
+    let(:current_directory) { File.dirname(__FILE__) }
+    let(:solar_generation_file_path) { '../data/01-solar_generation.csv' }
+
     it 'reads a CSV file and returns a CSV object' do
-      csv_object = CsvFileParser.read(File.join(File.dirname(__FILE__), '../data/01-solar_generation.csv'))
+      csv_object = CsvFileParser.read(File.join(current_directory, solar_generation_file_path))
+
       expect(csv_object).to be_instance_of(CSV)
     end
   end
